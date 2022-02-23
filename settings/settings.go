8ea3e203bbce3d1fcb8397163f1d08b5bcdba070
lxlan <lxlandsx@outlook.com>
2022-02-23 21:27:31 +0800

    第一次模板修改

--- settings/settings.go
+++ settings/settings.go
@@ -2,17 +2,59 @@ package settings
 
 import (
 	"fmt"
+
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
 )
+
+var Conf = new(AppConfig) // 用来保存程序的所有信息
+
+type AppConfig struct {
+	Name         string `mapstructure:"name"`
+	Mode         string `mapstructure:"mode"`
+	Version      string `mapstructure:"version"`
+	Port         int    `mapstructure:"port"`
+	*LogConfig   `mapstructure:"log"`
+	*MysqlConfig `mapstructure:"mysql"`
+	*RedisConfig `mapstructure:"redis"`
+}
+
+type LogConfig struct {
+	Level      string `mapstructure:"level"`
+	FileName   string `mapstructure:"filename"`
+	MaxSize    int    `mapstructure:"max_size"`
+	Max_Age    int    `mapstructure:"max_age"`
+	Max_Backup int    `mapstructure:"max_backup"`
+}
+
+type MysqlConfig struct {
+	Host        string `mapstructure:"host"`
+	Port        int    `mapstructure:"port"`
+	User        string `mapstructure:"user"`
+	Password    string `mapstructure:"password"`
+	Dbname      string `mapstructure:"dbname"`
+	MaxOpenConn int    `mapstructure:"max_open_conn"`
+	MaxIdleConn int    `mapstructure:"max_idle_conn"`
+}
+
+type RedisConfig struct {
+	host     string `mapstructure:"host"`
+	port     int    `mapstructure:"port"`
+	password string `mapstructure:"password"`
+	db       int    `mapstructure:"db"`
+	PoolSize int    `mapstructure:"pool_size"`
+}
 
 func Init() (err error) {
 	viper.SetConfigFile("../config.yaml")
 	viper.AddConfigPath("./")
 	err = viper.ReadInConfig() // 读取配置信息
-	viper.WatchConfig()        //检测
+	// 把读取到的配置文件反序列换到Conf变量中
+	viper.Unmarshal(Conf)
+	viper.WatchConfig() //检测
 	viper.OnConfigChange(func(in fsnotify.Event) {
 		fmt.Println("配置文件修改了")
+		viper.Unmarshal(Conf)
 	})
 	return
 }
